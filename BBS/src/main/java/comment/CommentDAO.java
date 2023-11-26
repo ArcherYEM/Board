6661e4229f0ba071e55c12a1d95e6e814dd1416b
Dev.Yem <134784917+ArcherYEM@users.noreply.github.com>
2023-11-27 00:37:11 +0900

    Update CommentDAO.java

--- BBS/src/main/java/comment/CommentDAO.java
+++ BBS/src/main/java/comment/CommentDAO.java
@@ -1,5 +1,54 @@
 package comment;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
 public class CommentDAO {
+
+	private Connection conn;
+	private ResultSet rs;
+
+	public CommentDAO() {
+		try {
+			String dbURL = "jdbc:mysql://localhost:3306/BBS";
+			String dbID = "root";
+			String dbPassword = "1234";
+			Class.forName("com.mysql.jdbc.Driver");
+			conn = DriverManager.getConnection(dbURL, dbID, dbPassword);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public String getDate() {
+		String SQL = "SELECT NOW()";
+		try {
+			PreparedStatement pstmt = conn.prepareStatement(SQL);
+			rs = pstmt.executeQuery();
+			if (rs.next()) {
+				return rs.getString(1);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return ""; // database error
+	}
+
+	public int addComment(String comment, String userID) {
+	    String SQL = "INSERT INTO comment (bbsID, userID, content) VALUES (?, ?, ?)";
+	    try {
+	        PreparedStatement pstmt = conn.prepareStatement(SQL);
+	        pstmt.setInt(1, 0); // 현재 bbsID 입력되게 수정해야함
+	        pstmt.setString(2, userID);
+	        pstmt.setString(3, comment);
+	        return pstmt.executeUpdate();
+	    } catch (Exception e) {
+	        e.printStackTrace();
+	    }
+	    return -1; // 데이터베이스 오류
+	}
+
 
 }
