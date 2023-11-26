ecb22ed854e030beb697cf62cb86e8c7989806c8
Dev.Yem <134784917+ArcherYEM@users.noreply.github.com>
2023-11-26 16:59:59 +0900

    Add files via upload

---
+++ BBS/src/main/java/comment/Comment.java
@@ -0,0 +1,44 @@
+package comment;
+
+public class Comment {
+	
+	private int bbsID;
+	private String userID;
+	private String content;
+	private String date;
+	private String commentavailable;
+	
+	public int getBbsID() {
+		return bbsID;
+	}
+	public void setBbsID(int bbsID) {
+		this.bbsID = bbsID;
+	}
+	public String getUserID() {
+		return userID;
+	}
+	public void setUserID(String userID) {
+		this.userID = userID;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public String getDate() {
+		return date;
+	}
+	public void setDate(String date) {
+		this.date = date;
+	}
+	public String getCommentavailable() {
+		return commentavailable;
+	}
+	public void setCommentavailable(String commentavailable) {
+		this.commentavailable = commentavailable;
+	}
+	
+	
+
+}
